cc8faa5ea7cb100e077423a681998f7e4ce30688
lilkgb <mcleanrs@comcast.net>
2019-05-03 16:45:03 -0700

    build out header for what i want it to look like

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components'
 
 function Header(){
+  const Div = styled.div`
+    width: 100%;
+    background-color: lightgray;
+    display: flex;
+    height: 50px;
+    justify-content: space-between;
+    align-items: center;
+    .left{
+      margin-left: 10px;
+    }
+    input {
+      height: 25px;
+    }
+    .right{
+      margin-right: 10px;
+    }
+  `
   return(
-    <div>
-      <h2>THIS IS A HEADER</h2>
-      <Link to='/'>Home</Link>
-    </div>
+    <Div>
+      <p class="left"><Link to='/'>logo links to home</Link></p>
+      <input placeholder="Search"/>
+      <p class="right">sign in/ create a account</p>
+    </Div>
   )
 }
 
