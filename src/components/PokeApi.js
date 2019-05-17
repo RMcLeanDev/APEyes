b182978279d3ec7e28db7694d6213cf5c5f8a508
lilkgb <mcleanrs@comcast.net>
2019-05-17 16:23:08 -0700

    do research on giphy api sense last commit. add possible examples i can do in pokeapi component

--- src/components/PokeApi.js
+++ src/components/PokeApi.js
@@ -4,7 +4,12 @@ function PokeApi(){
   return(
     <div>
       <h1>Poke API Home</h1>
-      
+      <p>possible examples</p>
+      <ul>
+        <li>search by pokemon</li>
+        <li>search by type</li>
+        <li>search by abilty</li>
+      </ul>
     </div>
   )
 }
