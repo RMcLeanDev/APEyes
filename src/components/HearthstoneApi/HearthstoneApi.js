00107c11f36bb60d667f4b24fcf8e1b6d330048f
lilkgb <mcleanrs@comcast.net>
2019-05-20 11:03:22 -0700

    add basic layout to hearthstoneList.js and add to hearthstoneApi.js

--- src/components/HearthstoneApi/HearthstoneApi.js
+++ src/components/HearthstoneApi/HearthstoneApi.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import HearthstoneList from './HearthstoneList';
 
 function HearthstoneApi(){
   return(
     <div>
       <h1>Hearthstone</h1>
+      <HearthstoneList/>
     </div>
   )
 }
---
+++ src/components/HearthstoneApi/HearthstoneList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HearthstoneList(){
+  return(
+    <div>
+      <h1>List</h1>
+    </div>
+  )
+}
+
+export default HearthstoneList;
