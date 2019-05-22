b5735889533b00062bd4250526a9cb89ab294ab2
Student <example@example.com>
2019-05-22 12:52:27 -0700

    add grid to all api component

--- src/components/AllApis.js
+++ src/components/AllApis.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import styled from 'styled-components'
 
 function AllApis(){
+  const Div = styled.div`
+    .imgs{
+      border: 1px solid red;
+      display: grid;
+      width: 98vw;
+      margin: auto;
+      grid-template-columns: calc(50% - 5px) calc(50% - 5px);
+      grid-gap: 10px;
+      img {
+        transition: .2s linear;
+        justify-self: center;
+        width: 100%;
+        &:hover{
+          box-shadow: 0px 0px 10px black;
+        }
+      }
+    }
+  `;
   return(
-    <div>
+    <Div>
       <h1>All Api's component</h1>
-      <div>
+      <div className='imgs'>
         <img src={require('../assets/apiimgs/giphyApiGif.gif')}/>
+        <img src={require('../assets/apiimgs/hearthstone.jpg')}/>
       </div>
-    </div>
+    </Div>
   )
 }
 
