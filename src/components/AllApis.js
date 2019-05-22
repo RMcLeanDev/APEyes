275493b509974525d00e9288f87c32de32ea215e
Student <example@example.com>
2019-05-22 12:07:21 -0700

    add giphyapi gif

--- src/components/AllApis.js
+++ src/components/AllApis.js
@@ -4,6 +4,9 @@ function AllApis(){
   return(
     <div>
       <h1>All Api's component</h1>
+      <div>
+        <img src={require('../assets/apiimgs/giphyApiGif.gif')}/>
+      </div>
     </div>
   )
 }
